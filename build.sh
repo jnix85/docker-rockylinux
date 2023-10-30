0eb2197304fbfbe7a00dc2d0a01e5567075a649a
Jason Parks <>
2023-10-30 17:41:33 -0500

    .

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 for i in 8 9
 do git checkout el${i}
-docker buildx build --platform linux/amd64,linux/arm64 --push --pull -t registry.pupgizmo.com/rockylinux/rockylinux:${i} -f Dockerfile . 
+docker buildx build --platform linux/amd64,linux/arm64 --push --pull -t registry.pupgizmo.com/rockylinux/${i}:latest -f Dockerfile . 
 done
 git checkout main
